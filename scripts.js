073cb7fc5e08fe27d9daec0a347b0a6c1b5d7355
riya636 <riyaaa.9099@gmail.com>
2025-01-12 19:48:30 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// JavaScript to handle button clicks or interactivity (Optional)
+
+document.querySelectorAll('.register').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Registration successful!');
+    });
+});
+
+document.querySelectorAll('.details').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('More details coming soon.');
+    });
+});
